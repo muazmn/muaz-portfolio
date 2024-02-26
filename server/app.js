b35b851ee19c91616ff50a1b3520db46fe7b0bb3
muazaiman45 <87751717+muazmn@users.noreply.github.com>
2024-02-26 14:47:55 +0300

    refactor nodemailer

--- server/app.js
+++ server/app.js
@@ -16,52 +16,51 @@ app.use((req, res, next) => {
 
 var myemail = process.env.EMAIL;
 var mypassword = process.env.PASSWORD
-function sendEmail({ receipient_email, subject, message }) {
-    return new Promise((resolve, reject) => {
-        // we use transporter to send the email
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                // refer the previous video to setup these both
-                user:myemail,
-                // the password is not from our gmail, but come from the application that permit us to use gmail to send these automatic email
-                pass: mypassword
-            },
-        });
-
-        const mail_configs = {
-            from: myemail,
-            to: myemail,
-            subject: subject,
-            text: message
-        };
-        transporter.sendMail(mail_configs, function(error, info) {
-            if(error) {
-                console.log(error);
-                return reject({ message: `An error has occured`})
-            }
-            return resolve({ message: 'Email sent successfully'});
-        });
-    });
+export default async (req,res)=> {
+    const { receipient_email, subject, message } = JSON.parse(req.body) 
+           // we use transporter to send the email
+            transporter = nodemailer.createTransport({
+               service: "gmail",
+               auth: {
+                   // refer the previous video to setup these both
+                   user:myemail,
+                   // the password is not from our gmail, but come from the application that permit us to use gmail to send these automatic email
+                   pass: mypassword
+               },
+           });
+   
+           const mail_configs = {
+               from: myemail,
+               to: myemail,
+               subject: subject,
+               text: message
+           };
+           await new Promise((resolve,reject) => {
+               transporter.sendMail(mail_configs, function(error, info) {
+                   if(error) {
+                       console.log(error);
+                       return reject({ message: `An error has occured`})
+                   }
+                   return resolve({ message: 'Email sent successfully'});
+               });
+           })
 }
+
 // app.get('/', (req,res) => {
 //     sendEmail()
 //         .then((response) => res.send(response.message))
 //         .catch((error) => res.status(500).send(error.message)
 //     )
 // })
-module.exports = (req, res) => {
-    sendEmail(req.body)
-        .then((response) => {
-            res.send(response.message)
-    })
-        .catch((error) => {
-            res.status(500).send(error.message)
-            console.log('error bro')
-        });
-}
-app.listen(port, () => {
-    console.log(`port is listening at http://localhost:${port}`)
-})
+// module.exports = async (req, res) => {
+//     await sendEmail(req.body)
+//         .then((response) => {
+//             res.send(response.message)
+//     })
+//         .catch((error) => {
+//             res.status(500).send(error.message)
+//             console.log('error bro')
+//         });
+// }
 
 // CORS = a module that allows us to transport solid information from front-end to back-end
